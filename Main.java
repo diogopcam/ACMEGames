97ee703cd8f34386c5d67aed96718824ae5f5c45
Diogo Camargo <diogpcam@gmail.com>
2023-10-22 15:33:50 -0300

    Criação dos métodos addJogo() e verificaJogo(), que identifica se existe um jogo com o mesmo nome do objeto que desejamos adicionar no ArrayList.

--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
+import java.util.ArrayList;
 import aplicacao.ACMEGames;
 import dados.Categoria;
 import dados.JogoEletronico;
 import dados.JogoTabuleiro;
-
+import dados.Ludoteca;
 public class Main {
-
 	public static void main(String[] args) {
+		Ludoteca ludoteca = new Ludoteca();
+
 		JogoEletronico jogoum = new JogoEletronico("God of War", 2000,900, "Playstation", Categoria.STR);
 		JogoTabuleiro jogodois = new JogoTabuleiro("Banco Imobiliario", 2000, 1345, 90);
+		JogoEletronico jogotres = new JogoEletronico("God of War", 2000,900, "Playstation", Categoria.STR);
 		//System.out.println(jogoum.getCategoria().getNome());
-		System.out.println(jogoum.getPrecoBase());
-		System.out.println(jogoum.calculaPrecoFinal());
+		// System.out.println(jogoum.getPrecoBase());
+		// System.out.println(jogoum.calculaPrecoFinal());
+
+		System.out.println(ludoteca.verificaJogo(jogoum));
+		System.out.println(ludoteca.verificaJogo(jogotres));
+		// System.out.println(ludoteca.addJogo(jogotres));
+
+     //	System.out.println(ludoteca.hasNext());
+		// ludoteca.addJogo(jogotres);
+		//ludoteca.verificaJogo(jogoum);
+		// ludoteca.verificaJogo(jogotres);
+
 	}
 }
--- dados/Ludoteca.java
+++ dados/Ludoteca.java
@@ -2,17 +2,16 @@ package dados;
 import java.util.ArrayList;
 
 public class Ludoteca implements Iterador {
-
 	private int contador;
-
-	private Jogo[] jogo;
-
-	public Ludoteca() {
-
+	private ArrayList<Jogo> listaJogos;
+	
+	public Ludoteca(){
+		contador = 0;
+		listaJogos = new ArrayList();
 	}
 
-	public boolean addJogo(Jogo jogo) {
-		return false;
+	public boolean addJogo(Jogo jogo){
+		return listaJogos.add(jogo);
 	}
 
 	public Jogo consultaPorNome(String nome) {
@@ -23,28 +22,34 @@ public class Ludoteca implements Iterador {
 		return null;
 	}
 
+	// 	//Cadastrar jogos eletrônicos: lê todos os dados de cada jogo eletrônico e, se o
+	// nome não for repetido no sistema, cadastra-o no sistema. Se o nome do jogo for
+	// repetido mostra a mensagem no formato: 1:Erro-jogo com nome repetido:
+	// nome do jogo
+	// Para cada jogo el
 
-	/**
-	 * @see dados.Iterador#reset()
-	 */
+	public boolean verificaJogo(Jogo jogochave){
+		if(listaJogos.isEmpty()) return addJogo(jogochave);
+		else{
+			for(Jogo jogo: listaJogos){
+				if(jogo.getNome().equals(jogochave.getNome())){
+					System.out.println("Cadastro não pode ser feito!");
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 	public void reset() {
 
 	}
 
-
-	/**
-	 * @see dados.Iterador#hasNext()
-	 */
 	public boolean hasNext() {
-		return false;
+	
 	}
 
-
-	/**
-	 * @see dados.Iterador#next()
-	 */
 	public Object next() {
 		return null;
 	}
-
 }
